c26ee3347bfbda2e47a59a814c53cfc7f62738d0
JOSEPH GRIFFITH - STUDENT <G00350112@gmit.ie>
2018-05-03 18:16:22 +0100

    Second Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { HttpModule }    from '@angular/http';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+ 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { InfoPage } from '../pages/info/info';
+import { Data } from '../providers/data';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+   InfoPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    InfoPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler}, Data
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,97 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Data } from '../../providers/data';
+import { InfoPage } from '../info/info';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+ 
+    @ViewChild('slides') slides: any;
+ 
+    hasAnswered: boolean = false;
+    score: number = 0;
+ 
+    slideOptions: any;
+    questions: any;
+    
+ 
+    
 
-  constructor(public navCtrl: NavController) {
+   
 
-  }
-
+    
+    constructor(public navCtrl: NavController, public dataService: Data) {
+ 
+    }
+ //function to load data from provider
+    ionViewDidLoad() {
+ 
+        this.slides.lockSwipes(true);
+ 
+        this.dataService.load().then((data) => {
+ 
+            data.map((question) => {
+ 
+                let originalOrder = question.answers;
+                question.answers = this.randomizeAnswers(originalOrder);
+                return question;
+ 
+            });    
+ 
+            this.questions = data;
+ 
+        });
+ 
+    }
+ //function for swapping slides
+    nextSlide(){
+        this.slides.lockSwipes(false);
+        this.slides.slideNext();
+        this.slides.lockSwipes(true);
+    }
+ 
+    selectAnswer(answer, question){
+ 
+        this.hasAnswered = true;
+        answer.selected = true;
+        
+ //counter on answer
+        if(answer.correct){
+            this.score++;
+        }
+ //function for timer to next slider after question is answered
+        setTimeout(() => {
+            this.hasAnswered = false;
+            this.nextSlide();
+            answer.selected = false;
+           
+        }, 200);
+    }
+ //function to randomize the position of the answers
+    randomizeAnswers(randomanswer: any[]): any[] {
+ 
+        for (let i = randomanswer.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            let temp = randomanswer[i];
+            randomanswer[i] = randomanswer[j];
+            randomanswer[j] = temp;
+        }
+ 
+        return randomanswer;
+ 
+    }
+ //f to restart quiz
+    restartQuiz() {
+        this.score = 0;
+        this.slides.lockSwipes(false);
+        this.slides.slideTo(1, 1000);
+        this.slides.lockSwipes(true);
+    }
+    //f to navigate to revision page
+    ReadInfo(){
+        this.navCtrl.push(InfoPage);
+    }
+     
 }
